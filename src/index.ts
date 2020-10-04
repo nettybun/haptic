7f2229be80212cd372d304620a23b1da655db95c
Gen Hames <hamesg@tutanota.de>
2020-10-03 17:01:59 -0700

    Bundling. This is kinda the reason I forked

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import { subscribe } from './s';
-import { h, api } from './h';
+// This bundle uses sinueux/s as the observer implementation. The TS aliases are
+// used to mark it as an external dependency in esbuild, allowing downstream
+// codebases to write:
 
-import type { Subject } from './s';
-import type { JSXInternal } from './jsx';
+// import { h } from 'sinueux';
+// import { s, computed } from 'sinueux/s';
+// ...
+
+// Note that developers don't import sinueux/h since it doesn't have extended
+// JSX definitions (below) that use sinueux/s. Bundlers will include sinuous/s
+// only once since this bundle references it rather than embedding. This also
+// caches well for unbundled (ESM-only) development (Snowpack, etc)
+
+import { subscribe } from 'sinueux/s';
+import { h, api } from 'sinueux/h';
+
+import type { Subject } from 'sinueux/s';
+import type { JSXInternal } from 'sinueux/jsx';
 
 api.subscribe = subscribe;
 
