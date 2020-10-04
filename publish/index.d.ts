3114d3b042dfd3eebc7af9026fc1b97e76a6f490
Gen Hames <hamesg@tutanota.de>
2020-10-03 17:34:04 -0700

    Still no luck with JSX. Types aren't appropriately routed

--- publish/index.d.ts
+++ publish/index.d.ts
@@ -3,15 +3,32 @@ import type { Subject } from './s';
 import type { JSXInternal } from './jsx';
 export { h, api };
 declare namespace h {
-    namespace JSX {
+    export namespace JSX {
+        type Element = HTMLElement
+
+        interface ElementAttributesProperty extends JSXInternal.ElementAttributesProperty {}
+        interface ElementChildrenAttribute extends JSXInternal.ElementChildrenAttribute {}
+        interface IntrinsicElements extends JSXInternal.IntrinsicElements {}
+
+        // Prevent children on components that don't declare them
         interface IntrinsicAttributes {
             children?: never;
         }
+
         type MaybeSubject<T> = T | Subject<T>;
         type AllowSubject<Props> = {
             [K in keyof Props]: MaybeSubject<Props[K]>;
         };
-        type SVGAttributes<Target extends EventTarget = SVGElement> = AllowSubject<JSXInternal.SVGAttributes<Target>>;
-        type HTMLAttributes<RefType extends EventTarget = EventTarget> = AllowSubject<JSXInternal.HTMLAttributes<RefType>>;
+
+        // Allow children on all DOM elements (not components, see above)
+        // ESLint will error for children on void elements like <img/>
+        type DOMAttributes<Target extends EventTarget>
+            = JSXInternal.DOMAttributes<Target> & { children?: unknown; }
+
+        type SVGAttributes<Target extends EventTarget = SVGElement>
+            = HTMLAttributes<Target> & AllowSubject<JSXInternal.SVGAttributes<Target>>
+
+        type HTMLAttributes<RefType extends EventTarget = EventTarget>
+            = DOMAttributes<RefType> & AllowSubject<JSXInternal.HTMLAttributes<RefType>>
     }
 }
