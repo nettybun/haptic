118ff438ab5a2f56b44ac2788a8bcc7f635637b0
Gen Hames <hamesg@tutanota.de>
2020-10-04 19:23:51 -0700

    TS working but TS+ESLint fails about an unsafe return value

--- publish/index.d.ts
+++ publish/index.d.ts
@@ -4,31 +4,30 @@ import type { JSXInternal } from './jsx';
 export { h, api };
 declare namespace h {
     export namespace JSX {
-        type Element = HTMLElement
-
-        interface ElementAttributesProperty extends JSXInternal.ElementAttributesProperty {}
-        interface ElementChildrenAttribute extends JSXInternal.ElementChildrenAttribute {}
-        interface IntrinsicElements extends JSXInternal.IntrinsicElements {}
+        type MaybeSubject<T> = T | Subject<T>
+        type AllowSubject<Props> = { [K in keyof Props]: MaybeSubject<Props[K]> }
 
         // Prevent children on components that don't declare them
-        interface IntrinsicAttributes {
-            children?: never;
-        }
-
-        type MaybeSubject<T> = T | Subject<T>;
-        type AllowSubject<Props> = {
-            [K in keyof Props]: MaybeSubject<Props[K]>;
-        };
+        interface IntrinsicAttributes { children?: never }
 
         // Allow children on all DOM elements (not components, see above)
         // ESLint will error for children on void elements like <img/>
         type DOMAttributes<Target extends EventTarget>
-            = JSXInternal.DOMAttributes<Target> & { children?: unknown; }
-
-        type SVGAttributes<Target extends EventTarget = SVGElement>
-            = HTMLAttributes<Target> & AllowSubject<JSXInternal.SVGAttributes<Target>>
+            = AllowSubject<
+                JSXInternal.DOMAttributes<Target>
+                & { children?: unknown }>
 
         type HTMLAttributes<RefType extends EventTarget = EventTarget>
-            = DOMAttributes<RefType> & AllowSubject<JSXInternal.HTMLAttributes<RefType>>
+            = AllowSubject<Omit<JSXInternal.HTMLAttributes<RefType>, 'style'>>
+              & { style?:
+                    | MaybeSubject<string>
+                    | { [key: string]: MaybeSubject<string | number> }
+                }
+
+        type SVGAttributes<Target extends EventTarget = SVGElement>
+            = AllowSubject<JSXInternal.SVGAttributes<Target>>
+
+        type IntrinsicElements
+            = AllowSubject<JSXInternal.IntrinsicElements>
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -25,18 +25,30 @@ export { h, api };
 // If developers want to use a different subscribe implementation, edit this
 declare namespace h {
   export namespace JSX {
-    interface IntrinsicAttributes {
-      children?: never;
-    }
-
     type MaybeSubject<T> = T | Subject<T>
     type AllowSubject<Props> = { [K in keyof Props]: MaybeSubject<Props[K]> }
+
+    // Prevent children on components that don't declare them
+    interface IntrinsicAttributes { children?: never }
+
+    // Allow children on all DOM elements (not components, see above)
+    // ESLint will error for children on void elements like <img/>
+    type DOMAttributes<Target extends EventTarget>
+      = AllowSubject<
+          JSXInternal.DOMAttributes<Target>
+          & { children?: unknown }>
+
+    type HTMLAttributes<RefType extends EventTarget = EventTarget>
+      = AllowSubject<Omit<JSXInternal.HTMLAttributes<RefType>, 'style'>>
+        & { style?:
+              | MaybeSubject<string>
+              | { [key: string]: MaybeSubject<string | number> }
+          }
 
     type SVGAttributes<Target extends EventTarget = SVGElement>
       = AllowSubject<JSXInternal.SVGAttributes<Target>>
 
-    // TODO: MaybeSubject<> for HTMLAttributes.style
-    type HTMLAttributes<RefType extends EventTarget = EventTarget>
-      = AllowSubject<JSXInternal.HTMLAttributes<RefType>>
+    type IntrinsicElements
+      = AllowSubject<JSXInternal.IntrinsicElements>
   }
 }
