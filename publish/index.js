781b6dc5504dbabf1a2c0a4e0e5aa8b071e17fed
Gen Hames <hamesg@tutanota.de>
2020-10-10 22:04:06 -0700

    Publish

--- publish/index.js
+++ publish/index.js
@@ -1,2 +1,2 @@
-import{subscribe as i}from"./s";import{h as t,svg as n,when as r,api as e}from"./h";e.subscribe=i;export{e as api,t as h,n as svg,r as when};
+import{subscribe as i,sample as s}from"./s";import{h as t,api as e,svg as n,when as r}from"./h";e.subscribe=i,e.sample=s;export{e as api,t as h,n as svg,r as when};
 //# sourceMappingURL=index.js.map
