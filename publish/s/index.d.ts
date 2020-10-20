bf7686322da1e237bb147ae980009036bb74d56d
Gen Hames <hamesg@tutanota.de>
2020-10-20 12:41:09 -0700

    Update d.ts for capture()

--- publish/s/index.d.ts
+++ publish/s/index.d.ts
@@ -7,7 +7,7 @@ declare type Base<T> = {
 declare type WritableSignal<T> = Base<T> & {
     (nextValue: T): T;
     cs: Set<ComputedSignal<X>>;
-    csRun: Set<ComputedSignal<X>> | undefined;
+    csRun?: Set<ComputedSignal<X>>;
     pending: T | [];
 };
 declare type ComputedSignal<T> = Base<T> & {
@@ -16,13 +16,13 @@ declare type ComputedSignal<T> = Base<T> & {
     csNested: ComputedSignal<X>[];
 };
 declare type Signal<T> = WritableSignal<T> | ComputedSignal<T>;
-
 declare function createWritableSignal<T>(value: T): WritableSignal<T>;
 declare function createComputedSignal<F extends Fn, T = ReturnType<F>>(fn: F): ComputedSignal<T>;
 declare function subscribe(fn: Fn): () => void;
 declare function unsubscribe(computed: ComputedSignal<X>): void;
+declare function capture(fn: (unsubscribe?: () => void) => unknown): void;
 declare function transaction<T>(fn: () => T): T;
 declare function sample<T>(fn: () => T): T;
 declare function on(signals: Signal<X>[], fn: Fn, options?: { onlyChanges: boolean; }): ComputedSignal<unknown>;
 export { Signal, WritableSignal, ComputedSignal };
-export { createWritableSignal as s, createWritableSignal as signal, createComputedSignal as c, createComputedSignal as computed, subscribe, unsubscribe, transaction, sample, on };
+export { createWritableSignal as s, createWritableSignal as signal, createComputedSignal as c, createComputedSignal as computed, capture, subscribe, unsubscribe, transaction, sample, on };
