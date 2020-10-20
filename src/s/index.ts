fe163dcb35320db2fcec5c7e3fae0adf8fbc2f05
Gen Hames <hamesg@tutanota.de>
2020-10-20 12:08:22 -0700

    Change on() default behaviour (⚠)

--- src/s/index.ts
+++ src/s/index.ts
@@ -159,12 +159,11 @@ function sample<T>(fn: () => T): T {
   return value;
 }
 
-function on(signals: Signal<X>[], fn: Fn, options = { onlyChanges: true }) {
-  signals = ([] as Signal<X>[]).concat(signals);
+function on(signals: Signal<X>[], fn: Fn, options = { onlyChanges: false }) {
   return createComputedSignal(() => {
     signals.forEach(signal => { signal(); });
     let value;
-    if (!options.onlyChanges) value = sample(fn);
+    if (options.onlyChanges) value = sample(fn);
     options.onlyChanges = false;
     return value;
   });
