96cdc403170da48ad0f2a07e5a0d13930c1a5270
Gen Hames <hamesg@tutanota.de>
2021-01-11 14:09:36 -0800

    Local "externals" aren't working in esbuild. Revert to inlining.
    However, I was using ESM (or a downstream bundler) to make sure we're always using only one instance of haptic/v, so...
    
    Need to look into how tree-shaking will work via "examples/" folder

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,69 @@
-// This bundle uses haptic/s as the observer implementation. Developers using
-// the library can load both h and s by writing:
+// Haptic is a bundle of haptic/h that looks for haptic/v at runtime to use as
+// the reactivity engine. You access haptic/v on its own:
 
 // import { h } from 'haptic';
-// import { s, computed } from 'haptic/s';
+// import { vocals, rx } from 'haptic/v';
 
-// Since the haptic package doesn't embed haptic/s, code will only be loaded
-// once despite having two import sites. This should work well for both bundlers
-// and unbundled (ESM-only; Snowpack/UNPKG) workflows. Note that if haptic/h
-// was imported instead, the JSX definitions wouldn't support haptic/s
+// This bundles two utilities, svg and when, and extends the JSX namespace to
+// allow using Vocal and Rx types in JSX. Import haptic/h to use a vanilla JSX
+// namespace or to use other reactivity libraries such as sinuous/observable,
+// haptic/s, hyperactiv, or mobx.
 
-import { rx } from './v';
+// TODO: Read haptic/s for ESM-related single instancing of globals...
+
 import { h, api } from './h';
+import { rx, adopt } from './v';
 
-import { svg } from './u/svg.js';
-import { when } from './u/when.js';
-
-import type { Vocal } from './v';
+import type { Rx, Vocal, VocalSubscriber } from './v';
 import type { GenericEventAttrs, HTMLAttrs, SVGAttrs, HTMLElements, SVGElements } from './jsx';
 
-api.reactiveFn = rx;
+type El = Element | Node | DocumentFragment | undefined;
+type Component = (...args: unknown[]) => El;
+
+// Expects haptic/v to have setup up globally on window?
+// -- api.rx = (...args) => window.haptic.rx(...args);
+
+// TODO: This doesn't work? Importing haptic/v separately will use different
+// variables for rxActive, rx.id counters, etc. It has to be the same...
+api.rx = rx;
+
+/** Utility: Renders SVGs by setting h() to the SVG namespace */
+const svg = <T extends () => Element>(closure: T): ReturnType<T> => {
+  const prev = api.ns;
+  api.ns = 'http://www.w3.org/2000/svg';
+  const el = closure();
+  api.ns = prev;
+  return el as ReturnType<T>;
+};
+
+/** Utility: Switches content when the vocal in `condition` is updated */
+const when = <T extends string>(
+  condition: (s: VocalSubscriber) => T,
+  views: { [k in T]?: Component }
+): (s: VocalSubscriber) => El => {
+  const renderedEl = {} as { [k in T]: El };
+  const renderedRx = {} as { [k in T]: Rx };
+  let condActive: T;
+  return s => {
+    const cond = condition(s);
+    if (cond !== condActive && views[cond]) {
+      // Tick. Pause reactions. Keep DOM intact.
+      renderedRx[condActive].pause();
+      condActive = cond;
+      // Rendered?
+      if (renderedEl[cond]) {
+        // Then unpause. If nothing has changed then no sr/pr links change
+        renderedRx[cond]();
+      }
+      // Able to render?
+      const parent = rx(() => {});
+      // TODO: ESM vs window globalThis to access correct rxActive?
+      renderedEl[cond] = adopt(parent, () => h(views[cond]));
+      renderedRx[cond] = parent;
+    }
+    return renderedEl[cond];
+  };
+};
 
 export { h, api, svg, when };
 
