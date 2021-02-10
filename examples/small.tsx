307f4850fa661964c81110db5f1fd591690d9566
Gen Hames <hamesg@tutanota.de>
2021-02-10 10:38:50 -0800

    Smaller bundle size with wire

--- examples/small.tsx
+++ examples/small.tsx
@@ -1,4 +1,4 @@
-// 1063 bytes as haptic
-// 991 bytes as haptic/h (since no reason to load rx)
+// 1056 bytes as haptic
+// 981 bytes as haptic/h (since no reason to load wire)
 import { h } from '../src';
 document.body.appendChild(<p>Haptic</p>);
