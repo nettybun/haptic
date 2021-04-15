1e5095893bd0a1f4a376f0556d668c62064b153d
Gen Hames <hamesg@tutanota.de>
2021-04-14 23:40:55 -0700

    Filed TS bug. In the meantime, use somewhat verbose `{ x(): type { return ... } }` syntax

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,5 +1,5 @@
 import { h, api } from '../src/index.js';
-// import { wS, wR } from '../src/w/index.js';
+import { wireSignals, wR } from '../src/w/index.js';
 import type { WireReactor, WireSignal, SubToken } from '../src/w/index.js';
 
 import {
@@ -8,77 +8,24 @@ import {
   regDebugTrackSignalSubscriptions
 } from './registryDebugging.js';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare type X = any;
-declare function wR<T>(fn: ($: SubToken) => T): WireReactor<T>;
-
-declare function wS1<T extends {
-  [K in keyof T]: (() => X) | T[K]
-}>(obj: T): {
-  [K in keyof T]: WireSignal<T[K] extends () => () => infer R ? R : T[K]>;
-}
-const data1 = wS1({
-  text: '',
-  count: 0,
-  countPlusOne: () => () => data1.count() + 1,
-  countPlusTwo: () => () => data1.countPlusOne() + 1,
-});
-
-// This works. Least verbose working version...
-declare function wS2<T extends {
-  [K in keyof T]: (($: SubToken) => X) | T[K]
-}>(obj: T): {
-  [K in keyof T]: WireSignal<T[K] extends ($: SubToken) => () => infer R ? R : T[K]>;
-}
-const data2 = wS2({
-  text: '',
-  count: 0,
-  countPlusOne: ($) => () => data2.count($) + 1,
-  countPlusTwo: ($) => () => data2.countPlusOne($) + 1,
-});
-
-// This works. Somewhat verbose...
-declare function wS3<T extends {
-  [K in keyof T]: (($: SubToken) => X) | T[K]
-}>(obj: T): {
-  [K in keyof T]: WireSignal<T[K] extends ($: SubToken) => infer R ? R : T[K]>;
-}
-const data3 = wS3({
+// I actually filed a bug TypeScript #43683
+const data = wireSignals({
   text: '',
   count: 0,
   countPlusOne($): number {
-    return data3.count($) + 1;
+    return data.count($) + 1;
   },
   countPlusTwo($): number {
-    return data3.countPlusOne($) + 1;
+    return data.countPlusOne($) + 1;
   },
 });
-
-// This works. Extremely verbose though...
-declare function wS4<T extends {
-  [K in keyof T]: (() => WireReactor<X>) | T[K]
-}>(obj: T): {
-  [K in keyof T]: WireSignal<T[K] extends () => WireReactor<infer R> ? R : T[K]>;
-}
-const data4 = wS4({
-  text: '',
-  count: 0,
-  countPlusOne(): WireReactor<number> {
-    return wR(($) => data4.count($) + 1);
-  },
-  countPlusTwo(): WireReactor<number> {
-    return wR(($) => data4.countPlusOne($) + 1);
-  },
-});
-
-const data = data4;
 
 // @ts-ignore
 window.data = data;
 
 // TODO: insert.patch(el, value) and property.patch(el, prop, value)
 api.patchHandler = regDebugPatchHandler;
-regDebugTrackSignalSubscriptions(Object.values(data));
+regDebugTrackSignalSubscriptions(Object.values(data) as WireSignal[]);
 
 const externallyDefinedReactorTest = wR(($) => {
   return `data.text chars: ${data.text($).length}; `
