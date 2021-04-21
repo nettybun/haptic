baf685605bac2d4e948eb9ddb17c26c0dda9af6c
Gen Hames <hamesg@tutanota.de>
2021-04-21 11:17:31 -0700

    Simplify patchTest/patchHandler into patch. 1563gz

--- src/h/index.ts
+++ src/h/index.ts
@@ -5,23 +5,27 @@ import { insert } from './nodeInsert.js';
 import { property } from './nodeProperty.js';
 import { remove } from './nodeRemove.js';
 
-const noop = () => {};
-
 // This API should be compatible with community libraries that extend Sinuous
-const api = {
-  /** Element namespace URL such as SVG or MathML */
-  ns: '',
+const api: {
   /** Hyperscript reviver */
-  h: _h,
+  h: typeof _h;
   // Customizable internal methods for h()
+  add: typeof add;
+  insert: typeof insert;
+  property: typeof property;
+  // Renamed for compatibility with Sinuous' community libraries
+  rm: typeof remove;
+  // Reactivity could be haptic/w, sinuous/observable, mobx, etc
+  patch: (expr: unknown, updateCallback?: (value: unknown) => void) => boolean,
+  /** Element namespace URL such as SVG or MathML */
+  ns?: string;
+} = {
+  h: _h,
   add,
   insert,
   property,
-  // Renamed for compatibility with Sinuous' community libraries
   rm: remove,
-  // Reactivity could be haptic/v, sinuous/observable, mobx, etc
-  patchTest: noop as unknown as (expr: unknown) => boolean,
-  patchHandler: noop as unknown as (expr: unknown, updateCallback: (value: unknown) => void) => void,
+  patch: () => false,
 };
 
 // Reference the latest internal h() allowing others to customize the call
