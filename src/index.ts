f53cdb734e957410af4177828e1581f7150cfd12
Gen Hames <hamesg@tutanota.de>
2021-04-21 01:38:22 -0700

    Smaller! 951gz hello world

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,13 @@
 // some shared global state that is setup during import.
 
 import { h, api } from './h';
-import { wR, adopt, reactorPause } from './w';
+import { wR, wS, adopt, reactorPause } from './w';
 
 import type { WireSignal, WireReactor } from './w';
 import type { GenericEventAttrs, HTMLAttrs, SVGAttrs, HTMLElements, SVGElements } from './jsx';
 
 type El = Element | Node | DocumentFragment;
 type Component = (...args: unknown[]) => El;
-
-// TODO: insert.patch(el, value) and property.patch(el, prop, value)
 
 api.patchTest = (expr) => (expr && (expr as { $wR: 1 }).$wR) as boolean;
 
@@ -34,44 +32,44 @@ api.patchHandler = (expr, updateCallback) => {
   (expr as WireReactor)();
 };
 
-/** Utility: Renders SVGs by setting h() to the SVG namespace */
-const svg = <T extends () => Element>(closure: T): ReturnType<T> => {
-  const prev = api.ns;
-  api.ns = 'http://www.w3.org/2000/svg';
-  const el = closure();
-  api.ns = prev;
-  return el as ReturnType<T>;
-};
+// /** Utility: Renders SVGs by setting h() to the SVG namespace */
+// const svg = <T extends () => Element>(closure: T): ReturnType<T> => {
+//   const prev = api.ns;
+//   api.ns = 'http://www.w3.org/2000/svg';
+//   const el = closure();
+//   api.ns = prev;
+//   return el as ReturnType<T>;
+// };
 
-/** Utility: Switches content when the vocal in `condition` is updated */
-const when = <T extends string>(
-  condition: WireSignal<T>,
-  views: { [k in T]?: Component }
-): WireReactor<El | undefined> => {
-  const renderedElements = {} as { [k in T]?: El };
-  const renderedReactors = {} as { [k in T]?: WireReactor<void> };
-  let condActive: T;
-  return wR(($) => {
-    const cond = condition($);
-    if (cond !== condActive && views[cond]) {
-      // Tick. Pause reactors and keep DOM intact
-      reactorPause(renderedReactors[condActive] as WireReactor);
-      condActive = cond;
-      // Rendered?
-      if (renderedElements[cond]) {
-        // Then unpause. If nothing has changed then no wR.rS/wR.rP links change
-        (renderedReactors[cond] as WireReactor)();
-      }
-      // Able to render?
-      const reactor = wR(() => {});
-      renderedElements[cond] = adopt(reactor, () => h(views[cond] as Component));
-      renderedReactors[cond] = reactor;
-    }
-    return renderedElements[cond];
-  });
-};
+// /** Utility: Switches content when the vocal in `condition` is updated */
+// const when = <T extends string>(
+//   condition: WireSignal<T>,
+//   views: { [k in T]?: Component }
+// ): WireReactor<El | undefined> => {
+//   const renderedElements = {} as { [k in T]?: El };
+//   const renderedReactors = {} as { [k in T]?: WireReactor<void> };
+//   let condActive: T;
+//   return wR(($) => {
+//     const cond = condition($);
+//     if (cond !== condActive && views[cond]) {
+//       // Tick. Pause reactors and keep DOM intact
+//       reactorPause(renderedReactors[condActive] as WireReactor);
+//       condActive = cond;
+//       // Rendered?
+//       if (renderedElements[cond]) {
+//         // Then unpause. If nothing has changed then no wR.rS/wR.rP links change
+//         (renderedReactors[cond] as WireReactor)();
+//       }
+//       // Able to render?
+//       const reactor = wR(() => {});
+//       renderedElements[cond] = adopt(reactor, () => h(views[cond] as Component));
+//       renderedReactors[cond] = reactor;
+//     }
+//     return renderedElements[cond];
+//   });
+// };
 
-export { h, api, svg, when };
+export { h, api, wS, wR };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type DistributeWireReactorType<T> = T extends any ? WireReactor<T> : never;
