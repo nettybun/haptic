30e435f1b8aeaa86b4dac95cdcdd6ac7a2e0e9b4
Gen Hames <hamesg@tutanota.de>
2021-05-10 10:05:10 -0700

    Build CJS for Node

--- build.js
+++ build.js
@@ -2,6 +2,22 @@ import esbuild from 'esbuild';
 import { gzip } from 'fflate';
 import { readFile, writeFile } from 'fs/promises';
 
+const entryPoints = [
+  'src/dom/index.ts',
+  'src/wire/index.ts',
+  'src/utils/index.ts',
+  'src/index.ts',
+];
+
+// About 100 characters saved this way
+const define = {
+  STATE_RESET: 0,
+  STATE_RUNNING: 1,
+  STATE_WIRED_WAITING: 2,
+  STATE_WIRED_PAUSED: 3,
+  STATE_WIRED_STALE: 4,
+};
+
 // This is explained in ./src/index.ts. Haptic's bundle entrypoint isn't a self
 // contained bundle. This is to support unbundled developement workflows that
 // are ESM-only. For production, your bundler can re-bundle it.
@@ -15,15 +31,8 @@ const externalPlugin = {
   },
 };
 
-const sourceComment = '\n//# sourceMappingURL=index.js.map';
-
 esbuild.build({
-  entryPoints: [
-    'src/dom/index.ts',
-    'src/wire/index.ts',
-    'src/utils/index.ts',
-    'src/index.ts',
-  ],
+  entryPoints,
   outdir: 'publish',
   plugins: [
     externalPlugin,
@@ -33,44 +42,53 @@ esbuild.build({
   sourcemap: true,
   minify: true,
   metafile: true,
-  // About 100 characters saved this way
-  define: {
-    STATE_RESET: 0,
-    STATE_RUNNING: 1,
-    STATE_WIRED_WAITING: 2,
-    STATE_WIRED_PAUSED: 3,
-    STATE_WIRED_STALE: 4,
-  },
-})
-  .then((build) => Promise.all(
-    Object.keys(build.metafile.outputs)
-      .filter((filePath) => !filePath.endsWith('.map'))
-      .map(async (file) => {
-        const readDataFull = await readFile(file);
-        const indexEndOfCode = readDataFull.length - sourceComment.length;
-        const readData = readDataFull.subarray(0, indexEndOfCode);
-        const writeData = await new Promise((res, rej) => {
-          gzip(readData, { consume: true, level: 9 }, (err, data) => {
-            if (err) rej(err);
-            writeFile(file + '.gz', data);
-            res(data);
-          });
+  define,
+}).then((build) => Promise.all(
+  Object.keys(build.metafile.outputs)
+    .filter((filePath) => !filePath.endsWith('.map'))
+    .map(async (file) => {
+      const readDataFull = await readFile(file);
+      const sourceComment = '\n//# sourceMappingURL=index.js.map';
+      const indexEndOfCode = readDataFull.length - sourceComment.length;
+      const readData = readDataFull.subarray(0, indexEndOfCode);
+      const writeData = await new Promise((res, rej) => {
+        gzip(readData, { consume: true, level: 9 }, (err, data) => {
+          if (err) rej(err);
+          // Emit the .gz file so webservers can serve that directly
+          writeFile(file + '.gz', data);
+          res(data);
         });
-        return {
-          file,
-          min: readData.length,
-          mingz: writeData.length,
-        };
-      })
-  ))
-  .then((sizeObjects) => {
-    sizeObjects.forEach(({ file, min, mingz }) => {
-      file = file.replace('publish/', '');
-      console.log(
-        `${file.padEnd(15)} min:${String(min).padEnd(5)} min+gzip:${mingz}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
+      });
+      return {
+        file,
+        min: readData.length,
+        mingz: writeData.length,
+      };
+    })
+)).then((sizeObjects) => {
+  sizeObjects.forEach(({ file, min, mingz }) => {
+    file = file.replace('publish/', '');
+    console.log(
+      `${file.padEnd(15)} min:${String(min).padEnd(5)} min+gzip:${mingz}`);
   });
+}).catch((err) => {
+  console.error('ESM', err);
+  process.exit(1);
+});
+
+// CommonJS for older Node and require()
+esbuild.build({
+  entryPoints,
+  entryNames: '[dir]/[name]-cjs',
+  outdir: 'publish',
+  plugins: [
+    externalPlugin,
+  ],
+  format: 'cjs',
+  bundle: true,
+  minify: true,
+  define,
+}).catch((err) => {
+  console.error('CJS', err);
+  process.exit(1);
+});
