8569abba947b16e31e8d3459d0ba38250ad34c83
Gen Hames <hamesg@tutanota.de>
2021-08-05 02:40:50 -0700

    I broke `when()` when I removed chaining. Need to replan

--- src/stdlib/index.ts
+++ src/stdlib/index.ts
@@ -12,7 +12,7 @@ const when = <T extends string>(
   views: { [k in T]?: Component }
 ): Wire<El | undefined> => {
   const activeEls = {} as { [k in T]?: El };
-  const activeWires = {} as { [k in T]: Wire<void> };
+  const activeWires = {} as { [k in T]?: Wire<void> };
   let condRendered: T;
   conditionWire.tasks.add((cond) => {
     // XXX: I can't believe this has been broken for 8 months...
@@ -20,12 +20,14 @@ const when = <T extends string>(
     if (cond === condRendered) return activeEls[cond];
     condRendered = cond;
     // Else, content is changing. Pause the current wires. Keep the DOM.
-    wirePause(activeWires[condRendered]);
+    if (activeWires[condRendered]) {
+      wirePause(activeWires[condRendered]!);
+    }
     // Have we rendered this new cond before? Then unpause the wire
     if (activeEls[cond]) {
       // Is it stale? Then run the wire
-      if (wireResume(activeWires[cond])) {
-        activeWires[cond]();
+      if (wireResume(activeWires[cond]!)) {
+        activeWires[cond]!();
       }
     } else {
       // Render the DOM tree from scratch and capture all nested wires
@@ -33,6 +35,8 @@ const when = <T extends string>(
       wireAdopt(wireRoot, () => activeEls[cond] = h(views[cond] as Component));
       activeWires[cond] = wireRoot;
     }
+    // TODO: BREAKING. Need to define a new wire and return that to get patched
+    // by api.patch... I broke chaining (on purpose).
     return activeEls[cond];
   });
   return conditionWire as unknown as Wire<El | undefined>;
